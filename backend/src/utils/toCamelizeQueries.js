1ececc878cfa7dac3f2ea070f0ab6bfa0c0cfadd
ElisaBorba <elisaborbaa@gmail.com>
2023-11-25 23:12:41 -0300

    fix snakeize path

--- backend/src/utils/toCamelizeQueries.js
+++ backend/src/utils/toCamelizeQueries.js
@@ -1,17 +1,17 @@
 // AO INSERIR DADOS ÀS TABELAS
 
-const snakeize = require('snakeize');
+// const snakeize = require('snakeize');
 
-const formattedColumnNames = (object) => Object.keys(snakeize(object)).join(',');
+// const formattedColumnNames = (object) => Object.keys(snakeize(object)).join(',');
 
-const formattedPlaceholders = (object) => Object.keys(object).map(() => '?').join(',');
+// const formattedPlaceholders = (object) => Object.keys(object).map(() => '?').join(',');
 
-const formattedUpdateColumns = (object) => Object.keys(snakeize(object))
-  .map((key) => `${key} = ?`)
-  .join(', ');
+// const formattedUpdateColumns = (object) => Object.keys(snakeize(object))
+//   .map((key) => `${key} = ?`)
+//   .join(', ');
 
-module.exports = {
-  formattedColumnNames,
-  formattedPlaceholders,
-  formattedUpdateColumns,
-};
+// module.exports = {
+//   formattedColumnNames,
+//   formattedPlaceholders,
+//   formattedUpdateColumns,
+// };
